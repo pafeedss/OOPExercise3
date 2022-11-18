4a651cc3f62288232a05e11aa8352bbc894e9ca3
Julius Marabe <116186260+pafeedss@users.noreply.github.com>
2022-11-18 20:05:52 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,20 @@
+public class Animal
+{
+	private String name;
+	
+	public Animal(){
+		name  ="Unknown";
+	}
+	public String getName(){
+		return name;
+	}
+	public String walkAnimal(){
+		
+		return "The dog animal Walked";
+		
+	}
+	public String stopAnimal(){
+		return "The dog animal has Stopped";
+	}
+	
+}
